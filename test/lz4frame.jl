39ab2a7b8c6b232b92ea434eeb8d9c3a1c92e1e8
Curtis Vogt <curtis.vogt@gmail.com>
2018-02-13 10:27:18 -0600

    Avoid including LZ4 source in tests

--- test/lz4frame.jl
+++ test/lz4frame.jl
@@ -1,85 +1,80 @@
-depsjl = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
-
-include(depsjl)
-include("../src/lz4frame.jl")
-
 @testset "lz4frame" begin
      testIn = "Far out in the uncharted backwaters of the unfashionable end of the west-
  ern  spiral  arm  of  the  Galaxy  lies  a  small  unregarded  yellow  sun."
     test_size = convert(UInt, length(testIn))
-    version = LZ4F_getVersion()
+    version = LZ4.LZ4F_getVersion()
 
     @testset "Errors" begin
         ERROR_GENERIC = UInt(18446744073709551615)
         no_error = UInt(0)
 
-        @test !LZ4F_isError(no_error)
-        @test LZ4F_getErrorName(no_error) == "Unspecified error code"
+        @test !LZ4.LZ4F_isError(no_error)
+        @test LZ4.LZ4F_getErrorName(no_error) == "Unspecified error code"
 
-        @test LZ4F_isError(ERROR_GENERIC)
-        @test LZ4F_getErrorName(ERROR_GENERIC) == "ERROR_GENERIC"
+        @test LZ4.LZ4F_isError(ERROR_GENERIC)
+        @test LZ4.LZ4F_getErrorName(ERROR_GENERIC) == "ERROR_GENERIC"
     end
 
     @testset "CompressionCtx" begin
-        ctx = Ref{Ptr{LZ4F_cctx}}(C_NULL)
+        ctx = Ref{Ptr{LZ4.LZ4F_cctx}}(C_NULL)
 
-        @test_nowarn err = LZ4F_createCompressionContext(ctx, version)
+        @test_nowarn err = LZ4.LZ4F_createCompressionContext(ctx, version)
         @test err == 0
 
-        @test_nowarn check_context_initialized(ctx[])
+        @test_nowarn LZ4.check_context_initialized(ctx[])
 
-        err = LZ4F_freeCompressionContext(ctx[])
+        err = LZ4.LZ4F_freeCompressionContext(ctx[])
         @test err == 0
-        @test !LZ4F_isError(err)
+        @test !LZ4.LZ4F_isError(err)
 
-        ctx = Ptr{LZ4F_cctx}(C_NULL)
-        @test_throws ErrorException check_context_initialized(ctx)
+        ctx = Ptr{LZ4.LZ4F_cctx}(C_NULL)
+        @test_throws ErrorException LZ4.check_context_initialized(ctx)
     end
 
 
     @testset "DecompressionCtx" begin
-        dctx = Ref{Ptr{LZ4F_dctx}}(C_NULL)
+        dctx = Ref{Ptr{LZ4.LZ4F_dctx}}(C_NULL)
 
-        @test_nowarn err = LZ4F_createDecompressionContext(dctx, version)
+        @test_nowarn err = LZ4.LZ4F_createDecompressionContext(dctx, version)
         @test err == 0
 
-        @test_nowarn check_context_initialized(dctx[])
+        @test_nowarn LZ4.check_context_initialized(dctx[])
 
-        @test_nowarn LZ4F_resetDecompressionContext(dctx[])
+        @test_nowarn LZ4.LZ4F_resetDecompressionContext(dctx[])
 
-        err = LZ4F_freeDecompressionContext(dctx[])
+        err = LZ4.LZ4F_freeDecompressionContext(dctx[])
         @test err == 0
 
-        dctx = Ptr{LZ4F_dctx}(C_NULL)
-        @test_throws ErrorException check_context_initialized(dctx)
-        @test_throws ErrorException LZ4F_resetDecompressionContext(dctx)
+        dctx = Ptr{LZ4.LZ4F_dctx}(C_NULL)
+        @test_throws ErrorException LZ4.check_context_initialized(dctx)
+        @test_throws ErrorException LZ4.LZ4F_resetDecompressionContext(dctx)
 
     end
 
     function test_decompress(origsize, buffer)
         @testset "Decompress" begin
-            dctx = Ref{Ptr{LZ4F_dctx}}(C_NULL)
+            dctx = Ref{Ptr{LZ4.LZ4F_dctx}}(C_NULL)
             srcsize = Ref{Csize_t}(origsize)
             dstsize =  Ref{Csize_t}(8*1280)
             decbuffer = Vector{UInt8}(1280)
 
-            frameinfo = Ref(LZ4F_frameInfo_t())
+            frameinfo = Ref(LZ4.LZ4F_frameInfo_t())
 
-            @test_nowarn err = LZ4F_createDecompressionContext(dctx, version)
+            @test_nowarn err = LZ4.LZ4F_createDecompressionContext(dctx, version)
 
-            @test_nowarn result = LZ4F_getFrameInfo(dctx[], frameinfo, buffer, srcsize)
+            @test_nowarn result = LZ4.LZ4F_getFrameInfo(dctx[], frameinfo, buffer, srcsize)
             @test srcsize[] > 0
 
             offset = srcsize[]
             srcsize[] = origsize - offset
 
-            @test_nowarn result = LZ4F_decompress(dctx[], decbuffer, dstsize, pointer(buffer) + offset, srcsize, C_NULL)
+            @test_nowarn result = LZ4.LZ4F_decompress(dctx[], decbuffer, dstsize, pointer(buffer) + offset, srcsize, C_NULL)
             @test srcsize[] > 0
 
             @test testIn == unsafe_string(pointer(decbuffer))
 
-            result = LZ4F_freeDecompressionContext(dctx[])
-            @test !LZ4F_isError(result)
+            result = LZ4.LZ4F_freeDecompressionContext(dctx[])
+            @test !LZ4.LZ4F_isError(result)
         end
 
     end
@@ -87,150 +82,150 @@ include("../src/lz4frame.jl")
     function test_invalid_decompress(origsize, buffer)
         @testset "DecompressInvalid" begin
 
-            dctx = Ref{Ptr{LZ4F_dctx}}(C_NULL)
+            dctx = Ref{Ptr{LZ4.LZ4F_dctx}}(C_NULL)
             srcsize = Ref{Csize_t}(origsize)
             dstsize =  Ref{Csize_t}(1280)
             decbuffer = Vector{UInt8}(1280)
 
-            frameinfo = Ref(LZ4F_frameInfo_t())
+            frameinfo = Ref(LZ4.LZ4F_frameInfo_t())
 
-            LZ4F_createDecompressionContext(dctx, version)
+            LZ4.LZ4F_createDecompressionContext(dctx, version)
 
-            buffer[1:LZ4F_HEADER_SIZE_MAX] = 0x10
-            @test_throws ErrorException LZ4F_getFrameInfo(dctx[], frameinfo, buffer, srcsize)
+            buffer[1:LZ4.LZ4F_HEADER_SIZE_MAX] = 0x10
+            @test_throws ErrorException LZ4.LZ4F_getFrameInfo(dctx[], frameinfo, buffer, srcsize)
 
             offset = srcsize[]
             srcsize[] = origsize - offset
 
-            @test_throws ErrorException LZ4F_decompress(dctx[], decbuffer, dstsize, pointer(buffer) + offset, srcsize, C_NULL)
+            @test_throws ErrorException LZ4.LZ4F_decompress(dctx[], decbuffer, dstsize, pointer(buffer) + offset, srcsize, C_NULL)
 
-            result = LZ4F_freeDecompressionContext(dctx[])
-            @test !LZ4F_isError(result)
+            result = LZ4.LZ4F_freeDecompressionContext(dctx[])
+            @test !LZ4.LZ4F_isError(result)
         end
     end
 
     @testset "Compress" begin
-        ctx = Ref{Ptr{LZ4F_cctx}}(C_NULL)
-        err = LZ4F_isError(LZ4F_createCompressionContext(ctx, version))
+        ctx = Ref{Ptr{LZ4.LZ4F_cctx}}(C_NULL)
+        err = LZ4.LZ4F_isError(LZ4.LZ4F_createCompressionContext(ctx, version))
         @test !err
 
-        prefs = Ptr{LZ4F_preferences_t}(C_NULL)
+        prefs = Ptr{LZ4.LZ4F_preferences_t}(C_NULL)
 
-        bound = LZ4F_compressBound(test_size, prefs)
+        bound = LZ4.LZ4F_compressBound(test_size, prefs)
         @test bound > 0
 
-        bufsize = bound + LZ4F_HEADER_SIZE_MAX
+        bufsize = bound + LZ4.LZ4F_HEADER_SIZE_MAX
         buffer = Vector{UInt8}(ceil(Int, bound / 8))
 
-        @test_nowarn result = LZ4F_compressBegin(ctx[], buffer, bufsize, prefs)
+        @test_nowarn result = LZ4.LZ4F_compressBegin(ctx[], buffer, bufsize, prefs)
 
         offset = result
-        @test_nowarn result = LZ4F_compressUpdate(ctx[], pointer(buffer) + offset, bufsize - offset, pointer(testIn), test_size, C_NULL)
+        @test_nowarn result = LZ4.LZ4F_compressUpdate(ctx[], pointer(buffer) + offset, bufsize - offset, pointer(testIn), test_size, C_NULL)
 
         offset += result
-        @test_nowarn result = LZ4F_flush(ctx[], pointer(buffer)+offset, bufsize - offset, C_NULL)
+        @test_nowarn result = LZ4.LZ4F_flush(ctx[], pointer(buffer)+offset, bufsize - offset, C_NULL)
 
         offset += result
-        @test_nowarn result = LZ4F_compressEnd(ctx[], pointer(buffer)+offset, bufsize - offset, C_NULL)
+        @test_nowarn result = LZ4.LZ4F_compressEnd(ctx[], pointer(buffer)+offset, bufsize - offset, C_NULL)
         @test result > 0
 
         offset += result
 
-        result = LZ4F_freeCompressionContext(ctx[])
-        @test !LZ4F_isError(result)
+        result = LZ4.LZ4F_freeCompressionContext(ctx[])
+        @test !LZ4.LZ4F_isError(result)
 
         test_decompress(offset, buffer)
         test_invalid_decompress(offset, buffer)
     end
 
     @testset "CompressUninitialized" begin
-        ctx = Ref{Ptr{LZ4F_cctx}}(C_NULL)
+        ctx = Ref{Ptr{LZ4.LZ4F_cctx}}(C_NULL)
 
-        prefs = Ptr{LZ4F_preferences_t}(C_NULL)
+        prefs = Ptr{LZ4.LZ4F_preferences_t}(C_NULL)
 
         bufsize = test_size
         buffer = Vector{UInt8}(test_size)
 
-        @test_throws ErrorException LZ4F_compressBegin(ctx[], buffer, bufsize, prefs)
-        @test_throws ErrorException LZ4F_compressUpdate(ctx[], pointer(buffer), bufsize, pointer(testIn), test_size, C_NULL)
-        @test_throws ErrorException LZ4F_flush(ctx[], pointer(buffer), bufsize, C_NULL)
-        @test_throws ErrorException LZ4F_compressEnd(ctx[], pointer(buffer), bufsize, C_NULL)
+        @test_throws ErrorException LZ4.LZ4F_compressBegin(ctx[], buffer, bufsize, prefs)
+        @test_throws ErrorException LZ4.LZ4F_compressUpdate(ctx[], pointer(buffer), bufsize, pointer(testIn), test_size, C_NULL)
+        @test_throws ErrorException LZ4.LZ4F_flush(ctx[], pointer(buffer), bufsize, C_NULL)
+        @test_throws ErrorException LZ4.LZ4F_compressEnd(ctx[], pointer(buffer), bufsize, C_NULL)
     end
 
     @testset "CompressInvalid" begin
-        ctx = Ref{Ptr{LZ4F_cctx}}(C_NULL)
-        LZ4F_createCompressionContext(ctx, version)
+        ctx = Ref{Ptr{LZ4.LZ4F_cctx}}(C_NULL)
+        LZ4.LZ4F_createCompressionContext(ctx, version)
 
-        prefs = Ptr{LZ4F_preferences_t}(C_NULL)
+        prefs = Ptr{LZ4.LZ4F_preferences_t}(C_NULL)
 
-        bound = LZ4F_compressBound(test_size, prefs)
+        bound = LZ4.LZ4F_compressBound(test_size, prefs)
         @test bound > 0
 
-        bufsize = bound + LZ4F_HEADER_SIZE_MAX
+        bufsize = bound + LZ4.LZ4F_HEADER_SIZE_MAX
         buffer = Vector{UInt8}(ceil(Int, bound / 8))
 
-        @test_throws ErrorException LZ4F_compressBegin(ctx[], buffer, UInt(2), prefs)
-        @test_throws ErrorException LZ4F_compressUpdate(ctx[], pointer(buffer), bufsize, pointer(testIn), test_size, C_NULL)
+        @test_throws ErrorException LZ4.LZ4F_compressBegin(ctx[], buffer, UInt(2), prefs)
+        @test_throws ErrorException LZ4.LZ4F_compressUpdate(ctx[], pointer(buffer), bufsize, pointer(testIn), test_size, C_NULL)
 
-        result = LZ4F_freeCompressionContext(ctx[])
-        @test !LZ4F_isError(result)
+        result = LZ4.LZ4F_freeCompressionContext(ctx[])
+        @test !LZ4.LZ4F_isError(result)
 
 
-        ctx = Ref{Ptr{LZ4F_cctx}}(C_NULL)
-        LZ4F_createCompressionContext(ctx, version)
+        ctx = Ref{Ptr{LZ4.LZ4F_cctx}}(C_NULL)
+        LZ4.LZ4F_createCompressionContext(ctx, version)
 
-        @test_nowarn result = LZ4F_compressBegin(ctx[], buffer, bufsize, prefs)
+        @test_nowarn result = LZ4.LZ4F_compressBegin(ctx[], buffer, bufsize, prefs)
 
         offset = result
-        @test_nowarn result = LZ4F_compressUpdate(ctx[], pointer(buffer) + offset, bufsize - offset, pointer(testIn), test_size, C_NULL)
+        @test_nowarn result = LZ4.LZ4F_compressUpdate(ctx[], pointer(buffer) + offset, bufsize - offset, pointer(testIn), test_size, C_NULL)
 
-        @test_throws ErrorException LZ4F_flush(ctx[], pointer(buffer), UInt(2), C_NULL)
-        @test_throws ErrorException LZ4F_compressEnd(ctx[], pointer(buffer), UInt(2), C_NULL)
+        @test_throws ErrorException LZ4.LZ4F_flush(ctx[], pointer(buffer), UInt(2), C_NULL)
+        @test_throws ErrorException LZ4.LZ4F_compressEnd(ctx[], pointer(buffer), UInt(2), C_NULL)
 
-        result = LZ4F_freeCompressionContext(ctx[])
-        @test !LZ4F_isError(result)
+        result = LZ4.LZ4F_freeCompressionContext(ctx[])
+        @test !LZ4.LZ4F_isError(result)
     end
 
     @testset "DecompressUninitialized" begin
-        dctx = Ref{Ptr{LZ4F_dctx}}(C_NULL)
+        dctx = Ref{Ptr{LZ4.LZ4F_dctx}}(C_NULL)
         srcsize = Ref{Csize_t}(test_size)
         dstsize =  Ref{Csize_t}(8*1280)
         decbuffer = Vector{UInt8}(1280)
 
-        frameinfo = Ref(LZ4F_frameInfo_t())
-        @test_throws ErrorException LZ4F_getFrameInfo(dctx[], frameinfo, pointer(testIn), srcsize)
-        @test_throws ErrorException LZ4F_decompress(dctx[], decbuffer, dstsize, pointer(testIn), srcsize, C_NULL)
+        frameinfo = Ref(LZ4.LZ4F_frameInfo_t())
+        @test_throws ErrorException LZ4.LZ4F_getFrameInfo(dctx[], frameinfo, pointer(testIn), srcsize)
+        @test_throws ErrorException LZ4.LZ4F_decompress(dctx[], decbuffer, dstsize, pointer(testIn), srcsize, C_NULL)
     end
 
     @testset "Preferences" begin
-        ctx = Ref{Ptr{LZ4F_cctx}}(C_NULL)
-        err = LZ4F_isError(LZ4F_createCompressionContext(ctx, version))
+        ctx = Ref{Ptr{LZ4.LZ4F_cctx}}(C_NULL)
+        err = LZ4.LZ4F_isError(LZ4.LZ4F_createCompressionContext(ctx, version))
         @test !err
-        opts = Ref(LZ4F_compressOptions_t(1, (0, 0, 0)))
-        prefs = Ref(LZ4F_preferences_t(LZ4F_frameInfo_t(), 20, 0, (0, 0, 0, 0)))
+        opts = Ref(LZ4.LZ4F_compressOptions_t(1, (0, 0, 0)))
+        prefs = Ref(LZ4.LZ4F_preferences_t(LZ4.LZ4F_frameInfo_t(), 20, 0, (0, 0, 0, 0)))
 
-        bound = LZ4F_compressBound(test_size, prefs)
+        bound = LZ4.LZ4F_compressBound(test_size, prefs)
         @test bound > 0
 
-        bufsize = bound + LZ4F_HEADER_SIZE_MAX
+        bufsize = bound + LZ4.LZ4F_HEADER_SIZE_MAX
         buffer = Vector{UInt8}(ceil(Int, bound / 8))
 
-        @test_nowarn result = LZ4F_compressBegin(ctx[], buffer, bufsize, prefs)
+        @test_nowarn result = LZ4.LZ4F_compressBegin(ctx[], buffer, bufsize, prefs)
 
         offset = result
-        @test_nowarn result = LZ4F_compressUpdate(ctx[], pointer(buffer) + offset, bufsize - offset, pointer(testIn), test_size, opts)
+        @test_nowarn result = LZ4.LZ4F_compressUpdate(ctx[], pointer(buffer) + offset, bufsize - offset, pointer(testIn), test_size, opts)
 
         offset += result
-        @test_nowarn result = LZ4F_flush(ctx[], pointer(buffer) + offset, bufsize - offset, opts)
+        @test_nowarn result = LZ4.LZ4F_flush(ctx[], pointer(buffer) + offset, bufsize - offset, opts)
 
         offset += result
-        @test_nowarn result = LZ4F_compressEnd(ctx[], pointer(buffer) + offset, bufsize - offset, opts)
+        @test_nowarn result = LZ4.LZ4F_compressEnd(ctx[], pointer(buffer) + offset, bufsize - offset, opts)
         @test result > 0
 
         offset += result
 
-        result = LZ4F_freeCompressionContext(ctx[])
-        @test !LZ4F_isError(result)
+        result = LZ4.LZ4F_freeCompressionContext(ctx[])
+        @test !LZ4.LZ4F_isError(result)
 
         test_decompress(offset, buffer)
     end
