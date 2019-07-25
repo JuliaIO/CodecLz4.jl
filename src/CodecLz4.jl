be47bb5a4fb6d794b1171f4582af78eab0054472
Sam Morrison <sam.morrison@invenia.ca>
2019-07-25 10:40:30 -0500

    Fixes minoutsize so large files no longer fail (#23)
    * Let error show backtrace
    
    * Update minoutsize to fix ERROR_dstMaxSize_tooSmall on large files
    
    * Bump version
    
    * Simplify showerror

--- src/CodecLz4.jl
+++ src/CodecLz4.jl
@@ -20,9 +20,7 @@ struct LZ4Exception <: Exception
     msg::AbstractString
 end
 
-function Base.showerror(io::IO, ex::LZ4Exception, bt; backtrace=false)
-    printstyled(io, "$(ex.src): $(ex.msg)", color=Base.error_color())
-end
+Base.showerror(io::IO, ex::LZ4Exception) = print(io, "$(ex.src): $(ex.msg)")
 
 include("lz4frame.jl")
 include("stream_compression.jl")
